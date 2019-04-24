0e0e1eae44185bcc85dcac51b8ebbe9181b8817d
Shank <shank@Nomans-MacBook-Pro.local>
2019-04-23 21:26:18 -0500

    Updates

---
+++ precache-manifest.725eb570f503bb27a978b1566c5f9279.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a9b24b68e8f497111c9dc5ab957381",
+    "url": "/clickygame/./index.html"
+  },
+  {
+    "revision": "131d5631cdf84fb53874",
+    "url": "/clickygame/./static/css/main.b5839f1c.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/./static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "131d5631cdf84fb53874",
+    "url": "/clickygame/./static/js/main.7f70e544.chunk.js"
+  },
+  {
+    "revision": "c5df5aba59d3bf4c0b14",
+    "url": "/clickygame/./static/js/runtime~main.0d2a9578.js"
+  }
+]);
